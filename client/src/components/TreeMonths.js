55033f4a2414ba3cb6573c699f77d0946ea9a445
roconn <roconn25@gmail.com>
2023-03-11 19:57:10 +0000

    altered output of form data

--- client/src/components/TreeMonths.js
+++ client/src/components/TreeMonths.js
@@ -1,77 +1,91 @@
 import React, { useState, useEffect } from "react";
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 function TreeMonths() {
+  const [formValues, setFormValues] = useState([{ month: "", number: "" }]);
 
-const [formValues, setFormValues] = useState([{ month: "", number: ""}])
-
-let handleChange = (i, e) => {
+  let handleChange = (i, e) => {
     let newFormValues = [...formValues];
     newFormValues[i][e.target.name] = e.target.value;
     setFormValues(newFormValues);
-  }
+  };
 
-let addFormFields = () => {
-    validate([...formValues, { month: "", number: "" }])
-    setFormValues([...formValues, { month: "", number: "" }])
+  let addFormFields = () => {
+    validate([...formValues, { month: "", number: "" }]);
+    setFormValues([...formValues, { month: "", number: "" }]);
+  };
 
-  }
-
-let removeFormFields = (i) => {
+  let removeFormFields = (i) => {
     let newFormValues = [...formValues];
     newFormValues.splice(i, 1);
-    setFormValues(newFormValues)
-}
+    setFormValues(newFormValues);
+  };
 
-let handleSubmit = (event) => {
+  let handleSubmit = (event) => {
     event.preventDefault();
-    alert(JSON.stringify(formValues));
-}
+    console.log(JSON.stringify(formValues), formValues[0].month, formValues[0].number)
+    return { formValues }
+  };
 
-// ! NOT CURRENTLY WORKING
-let validate = (...formValues) => {
-        if (formValues.number > 55) {
-            alert('55 is the maximum number!')
-            setFormValues(formValues.number = 1)
-        }
-
-        // if (number === 0) {
-        //     alert('Value cannot be zero!')
-        //     setFormValues(number = 1)
-        // }
+  // ! NOT CURRENTLY WORKING
+  let validate = (...formValues) => {
+    if (formValues.number > 55) {
+      alert("55 is the maximum number!");
+      setFormValues((formValues.number = 1));
     }
 
+    // if (number === 0) {
+    //     alert('Value cannot be zero!')
+    //     setFormValues(number = 1)
+    // }
+  };
 
-return (
-    <form  onSubmit={handleSubmit}>
+  return (
+    <form onSubmit={handleSubmit}>
       {formValues.map((element, index) => (
         <div className="form-inline" key={index}>
           <label>Month</label>
-          <input type="month" name="month" required={true} value={element.month || ""} onChange={e => handleChange(index, e)} />
+          <input
+            type="month"
+            name="month"
+            required={true}
+            value={element.month || ""}
+            onChange={(e) => handleChange(index, e)}
+          />
           <label>Tree Amount</label>
-          <input type="number" name="number" minLength={0} maxLength={55} value={element.number || ""} onChange={e => handleChange(index, e)} />
-          {
-            index ? 
-              <button type="button"  className="button remove" onClick={() => removeFormFields(index)}>Remove</button> 
-            : null
-          }
+          <input
+            type="number"
+            name="number"
+            minLength={0}
+            maxLength={55}
+            value={element.number || ""}
+            onChange={(e) => handleChange(index, e)}
+          />
+          {index ? (
+            <button
+              type="button"
+              className="button remove"
+              onClick={() => removeFormFields(index)}
+            >
+              Remove
+            </button>
+          ) : null}
         </div>
       ))}
       <div className="button-section">
-          <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
-          <button className="button submit" type="submit">Submit</button>
+        <button
+          className="button add"
+          type="button"
+          onClick={() => addFormFields()}
+        >
+          Add
+        </button>
+        <button className="button submit" type="submit">
+          Submit
+        </button>
       </div>
-  </form>
-)
+    </form>
+  );
 }
 
-
-
-
-
-
-
-
-
-export default TreeMonths
-
+export default TreeMonths;
