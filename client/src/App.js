52753022d1e0153cd1e46d4073bb085836bf7265
roconn <roconn25@gmail.com>
2023-03-11 14:49:03 +0000

    removed package + added axios, changed app layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,95 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import InputForm from './components/Input';
-import Selector from './components/Selector';
-import TableOutput from "./components/Table";
+
+
 
 function App() {
+  //! Select country
+  // sends post req to server which converts carbon amount to Kg and returns data
+  let [carbonAmount, setCarbon] = useState(0);
 
+  // api call
+  const sendData = (params) => {
+    fetch("/api/calculator", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        params,
+      }),
+    })
+      .then(console.log(params.country, params.simulationMode))
+      .then((res) => {
+        if (res.ok) {
+          console.log("^ Sending Receiving V");
+          return res.json();
+        } else {
+          console.log("It not work");
+        }
+      })
+      // .then(returnData => console.log(returnData))
+      .then((returnData) => setCarbon(returnData))
+      // .then(res => setCarbon(res.carbon))
 
-// set res as variables
+      .catch((error) => console.log(error));
 
-// empty states for data returned on form submits
+    setCarbon();
+  };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (formData) => {
+    console.log(formData);
+    console.log(errors);
 
+    // let params = {
+    //     country: formData.country,
+    //     simMode: formData.simulationMode
+    // }
 
+    let params = formData;
+
+    sendData(params);
+  };
+
+  //   DATA FOR STATE GOES HERE
+  console.log(carbonAmount);
+
+  // set res as variables
+
+  // empty states for data returned on form submits
 
   return (
     <div>
-      <Selector />
-      {/* <InputForm /> */}
-      <TableOutput />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <select {...register("country")}>
+        <option value="Australia">Australia</option>
+        <option value="China">China</option>
+        <option value="Germany">Germany</option>
+        <option value="India">India</option>
+        <option value="Singapore">Singapore</option>
+        <option value="South Africa">South Africa</option>
+        <option value="United Kingdom">United Kingdom</option>
+        <option value="United States">United States</option>
+      </select>
+      <select {...register("simulationMode")}>
+        <option value="Monthly">Monthly</option>
+        <option value="Yearly">Yearly</option>
+      </select>
+
+      <input type="submit" />
+    </form>
+
+
+
     </div>
-    
-  )
+   
+  );
 }
 
 export default App
